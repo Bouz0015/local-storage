bb22cfc7b2e9dfd5ccf5a29c8c87ed0964bc73a9
Aymane Bouzoubaa <aymanebouzoubaa@Aymanes-MBP.lan>
2018-01-02 12:57:12 -0500

    Local Storage

---
+++ JS/starter.js
@@ -0,0 +1,17 @@
+let contactStarter = [
+    {
+        "fullname": "Aymane Bouzoubaa",
+        "phone": "613-501-7623",
+        "email": "bouz0015@algonquinlive.com"
+    },
+    {
+        "fullname": "Joe",
+        "phone": "",
+        "email": ""
+    },
+    {
+        "fullname": "Jim",
+        "phone": "613-234-1234",
+        "email": "jim@gmail.com"
+    }
+]
